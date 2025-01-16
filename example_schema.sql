35fdcf91e8bbc35f3a757900aecbb9c8d868d225
manziprofessor <manziprofessor@gmail.com>
2025-01-16 16:31:50 +0200

    added database schema

--- example_schema.sql
+++ example_schema.sql
@@ -1,10 +1,19 @@
-drop table if exists examples;
-
-
-create table examples
-(
-    id   serial
-        constraint examples_pk
-            primary key,
-    name varchar
+-- Schema for places table
+CREATE TABLE places (
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(100),
+    county VARCHAR(100),
+    country VARCHAR(100)
 );
+
+-- Schema for people table
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    date_of_birth DATE,
+    city_of_birth VARCHAR(100),
+    place_id INT,
+    FOREIGN KEY (place_id) REFERENCES places(id)
+);
+
